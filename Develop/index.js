9bc33a9543bb79f867392030f17afa519d05a03e
Hmlessard <89013364+Hmlessard@users.noreply.github.com>
2021-10-30 14:01:34 -0500

    added license badges

--- Develop/index.js
+++ Develop/index.js
@@ -72,15 +72,15 @@ function userPrompts() {
 //Choosing a license function, based on user input
 function licenseType(response) {
     if (response.license == "MIT"){
-        licenseType = "[![License: MIT](https://choosealicense.com/licenses/apache-2.0/)";
+        licenseType = "[![License: MIT](https://img.shields.io/badge/MIT-MITLicense-brightgreen)](https://choosealicense.com/licenses/apache-2.0/)";
     } if (response.license == "GNU GPLv3"){
-        licenseType = "[![License: GNU GPLv3](https://choosealicense.com/licenses/agpl-3.0/";
+        licenseType = "[![License: GNU GPLv3](https://img.shields.io/badge/GNU%20GPLv3-GNU%20GPLv3License-yellowgreen)](https://choosealicense.com/licenses/agpl-3.0/";
     } if (response.license == "Mozilla Public License 2.0"){
-        licenseType = "[![License: Mozilla Public License 2.0](https://choosealicense.com/licenses/mpl-2.0/)";
+        licenseType = "[![License: Mozilla Public License 2.0](https://img.shields.io/badge/MozillaPublicLicense-MPL%203.0-orange)](https://choosealicense.com/licenses/mpl-2.0/)";
     } if (response.license == "Apache License 2.0"){
-        licenseType = "[![License: Apache License 2.0](https://choosealicense.com/licenses/apache-2.0/";
+        licenseType = "[![License: Apache License 2.0](https://img.shields.io/badge/ApacheLicense-Apache2.0-red)](https://choosealicense.com/licenses/apache-2.0/";
     } if (response.license == "Boost Software License 1.0"){
-        licenseType = "[![License: Boost Software License 1.0](https://choosealicense.com/licenses/bsl-1.0/";
+        licenseType = "[![License: Boost Software License 1.0](https://img.shields.io/badge/Boost%20Software-Boost%20Software%20License%201.0-blue)](https://choosealicense.com/licenses/bsl-1.0/";
     } else return;
     return licenseType;
     }
